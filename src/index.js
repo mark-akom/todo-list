b2701e20a1996a9c6f211ac056b251d4f70dde2d
mark-akom <mark.akom2019@gmail.com>
2022-01-17 09:41:01 +0000

    Added styling to the header section and also import the css file in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { todoApp } from "./modules/app";
 import createTopBar from './UI/navbar';
 
+import './UI/styles.css';
+
 const appBody = document.querySelector('#app-body');
 
 appBody.appendChild(createTopBar());
